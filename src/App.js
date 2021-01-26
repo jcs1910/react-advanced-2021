80bf37a2299e1fdb5dbd577d1f76b9a5a148ddb3
dokdo <jcs191072@gmail.com>
2021-01-26 14:29:11 +0900

    [ADD] -- Implement a basic useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import ErrorExample from './tutorial/1-useState/setup/1-error-example';
+import UseStateBasics from './tutorial/1-useState/final/2-useState-basics';
 
 function App() {
   return (
     <div className='container'>
-      <ErrorExample />
+      <UseStateBasics />
     </div>
   );
 }
---
+++ src/tutorial/1-useState/final/2-useState-basics.js
@@ -0,0 +1,25 @@
+import React, { useState, Fragment } from 'react';
+
+const UseStateBasics = () => {
+  let title = 'welcome the space';
+  const [text, setText] = useState(title);
+
+  const handleClick = () => {
+    if (text === 'welcome the space') {
+      setText('welcome Korea');
+    } else {
+      setText('welcome the space');
+    }
+  };
+
+  return (
+    <Fragment>
+      <h1>{text}</h1>
+      <button className='btn' onClick={handleClick}>
+        Change The Title
+      </button>
+    </Fragment>
+  );
+};
+
+export default UseStateBasics;
